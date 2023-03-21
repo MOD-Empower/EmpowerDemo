79c638d524f092581f8f60ba3871bdd7cfde6cda
Michiel Oda <michiel.oda@skyline.be>
2023-03-21 15:01:02 +0100

    Add project files.

---
+++ EmpowerDemo_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerDemo_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerDemo_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C6F1DDDC-55B6-4E84-A58C-20E27F67CBFA")]
+[assembly: AssemblyVersion("1.0.0.0")]
