8bf3fa2ec2332184070a0fbdc37891cd9630dcab
Michiel Oda <michiel.oda@skyline.be>
2023-03-22 14:11:19 +0100

    Adding 2nd script

---
+++ EmpowerDemo2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerDemo2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerDemo2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A2DCD003-1DAF-4BC6-ADAF-C2BE75BD05ED")]
+[assembly: AssemblyVersion("1.0.0.0")]
