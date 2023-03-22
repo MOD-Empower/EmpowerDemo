9ca832c1e95bf110ab87d8d0ca37021fc3fc8144
Michiel Oda <michiel.oda@skyline.be>
2023-03-22 15:11:54 +0100

    Add 3rd demo script

---
+++ EmpowerDemo3_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerDemo3_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerDemo3_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("F82E27BA-D1A9-4B1E-87E2-4DD207BE8621")]
+[assembly: AssemblyVersion("1.0.0.0")]
